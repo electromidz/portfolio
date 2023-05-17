818300dce5fd7f657e1a13d89a65a249b51d8b5f
electromidz <electromidz@gmail.com>
2023-05-17 16:16:51 +0330

    Work in next js

--- src/component/footer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../css/style.css';
-
-const Footer = () => {
-    return <div className="fter">footer</div>
-}
-
-export default Footer;
--- src/component/header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import '../css/style.css';
-import '../images/omid.jpg';
-
-const Header = () => {
-    return <header>Omid Moghadas</header>
-}
-
-export default Header;
