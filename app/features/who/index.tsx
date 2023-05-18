a05073de8a34e0390f607ea449d1665b03afdafb
electromidz <electromidz@gmail.com>
2023-05-18 19:02:25 +0330

    Need dynamic content on li

--- app/features/who/index.tsx
+++ app/features/who/index.tsx
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <div className="h-screen snap-center">who</div>;
+import Image from "next/image";
+
+export default function Who() {
+  return (
+    <div className="h-screen snap-center  w-8/12 m-auto">
+      <div className="flex flex-row space-between h-screen snap-center">
+        <div className="flex-1">{/* 3D model */}</div>
+        <div className="flex flex-1 flex-col justify-center gap-6 ">
+          <h1 className="text-4xl font-bold">Think out of square space</h1>
+          <div className="flex justify-start gap-5">
+            <span className="w-12 bg-red p-1 border-gray-200 dark:bg-gray-900"></span>
+            <span className="font-medium text-xl">Who we are</span>
+          </div>
+          <p className="font-light text-lg">
+            A creative person of designing and developing with a passion for the
+            arts.
+          </p>
+          <div className="flex justify-start">
+            <button className="font-bold text-lg">See my work</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- app/features/works/index.tsx
+++ app/features/works/index.tsx
@@ -1,3 +1,27 @@
-export default function Home() {
-  return <div className="h-screen snap-center">work</div>;
+export default function Work() {
+  const data = [
+    "Web Design",
+    "Development",
+    "Illustration",
+    "Product Design",
+    "Social Media",
+  ];
+
+  return (
+    <div className="h-screen snap-center  w-8/12 m-auto">
+      <div className="flex flex-row space-between h-screen snap-center">
+        <div className="flex flex-1 flex-col justify-center gap-6 ">
+          <ul className="list-none flex flex-col gap-8">
+            {data.map((item: string) => (
+              <li className="text-7xl font-bold cursor-pointer" key={item}>
+                {item}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="flex-1">{/* 3D model */}</div>
+      </div>
+    </div>
+  );
 }
