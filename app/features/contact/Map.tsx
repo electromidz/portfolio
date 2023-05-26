c4e62ecd2217d9211e5c9be4cda3dcaed5a8d5af
electromidz <electromidz@gmail.com>
2023-05-26 23:25:40 +0330

    Map have problem

--- app/features/contact/Map.tsx
+++ app/features/contact/Map.tsx
@@ -9,41 +9,42 @@ import {
 
 const Map = () => {
   return (
-    <ComposableMap
-      projection="geoAzimuthalEqualArea"
-      projectionConfig={{
-        rotate: [-10.0, -52.0, 0],
-        center: [-5, -3],
-        scale: 1100,
-      }}
-    >
-      <Geographies
-        geography="/features.json"
-        fill="#D6D6DA"
-        stroke="#FFFFFF"
-        strokeWidth={0.5}
-      >
-        {({ geographies }: any) =>
-          geographies.map((geo: any) => (
-            <Geography key={geo.rsmKey} geography={geo} />
-          ))
-        }
-      </Geographies>
-      {/* <Annotation
-        subject={[2.3522, 48.8566]}
-        dx={-90}
-        dy={-30}
-        connectorProps={{
-          stroke: "#FF5533",
-          strokeWidth: 3,
-          strokeLinecap: "round",
-        }}
-      >
-        <text x="-8" textAnchor="end" alignmentBaseline="middle" fill="#F53">
-          {"Paris"}
-        </text>
-      </Annotation> */}
-    </ComposableMap>
+    <></>
+    // <ComposableMap
+    //   projection="geoAzimuthalEqualArea"
+    //   projectionConfig={{
+    //     rotate: [-10.0, -52.0, 0],
+    //     center: [-5, -3],
+    //     scale: 1100,
+    //   }}
+    // >
+    //   <Geographies
+    //     geography="/features.json"
+    //     fill="#D6D6DA"
+    //     stroke="#FFFFFF"
+    //     strokeWidth={0.5}
+    //   >
+    //     {({ geographies }: any) =>
+    //       geographies.map((geo: any) => (
+    //         <Geography key={geo.rsmKey} geography={geo} />
+    //       ))
+    //     }
+    //   </Geographies>
+    //   <Annotation
+    //     subject={[2.3522, 48.8566]}
+    //     dx={-90}
+    //     dy={-30}
+    //     connectorProps={{
+    //       stroke: "#FF5533",
+    //       strokeWidth: 3,
+    //       strokeLinecap: "round",
+    //     }}
+    //   >
+    //     <text x="-8" textAnchor="end" alignmentBaseline="middle" fill="#F53">
+    //       {"Paris"}
+    //     </text>
+    //   </Annotation>
+    // </ComposableMap>
   );
 };
 
